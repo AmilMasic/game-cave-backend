25fd5a7253acbfc20f0a346345c02ba20537378f
AmilMasic <amil.masic@gmail.com>
2021-05-17 17:34:08 -0600

    stubbing out publisher controller

--- app/controllers/api/v1/publishers_controller.rb
+++ app/controllers/api/v1/publishers_controller.rb
@@ -1,2 +1,29 @@
 class API::V1::PublishersController < ApplicationController
+
+  def index
+    @publishers = Publisher.all
+    render json: @publishers
+  end
+
+  def create
+    @publisher = Publisher.new(publisher_params)
+    if @publisher.save
+      render json: @publisher
+    else
+      render json: { error: 'Error creating publisher.'}
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def publisher_params
+    params.require(publisher).permit(name, established, gamestore)
+  end
 end
