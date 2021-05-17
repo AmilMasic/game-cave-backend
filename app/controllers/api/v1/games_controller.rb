f0be9afe53b1ee6fb88d223e1b82bb83dd285ac1
AmilMasic <amil.masic@gmail.com>
2021-05-17 17:43:53 -0600

    stubbed out games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,2 +1,32 @@
 class API::V1::GamesController < ApplicationController
+
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      render json: @game
+    else
+      render json: { error: 'Error creating game.'}
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+  end
+
+  private
+
+  def game_params
+    params.require(game).permit(title, release_date, completed, genre, description, platform, publisher_id)
+  end
+
 end
