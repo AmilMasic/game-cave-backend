d8a99cab575bdd6fddc74aad823e77258394c3e8
AmilMasic <amil.masic@gmail.com>
2021-05-20 10:09:41 -0600

    added games as a nested route for publishers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :games
-      resources :publishers
+      resources :publishers do
+        resources :games
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
